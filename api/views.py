353c4690634e7c81c0726db100bc8d932d8523f0
Candykiller16 <Candykiller16@mail.ru>
2022-04-12 12:13:43 +0300

    JWT for access first installation and testing in Postman

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .serializers import ProjectSerializer
 from projects.models import Project
 
