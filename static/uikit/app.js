a879a3faba1c2e5d9f5171c1ffd7e9fdfffcc74a
Candykiller16 <candykiller16@mail.ru>
2022-05-18 21:35:13 +0300

    fix 1.0

--- static/uikit/app.js
+++ static/uikit/app.js
@@ -1,15 +1,17 @@
 // Invoke Functions Call on Document Loaded
 document.addEventListener('DOMContentLoaded', function () {
-  hljs.highlightAll();
+    hljs.highlightAll();
 
 });
 
 
-let alertWrapper = document.getElementsByClassName('.alert')
+let alertWrapper = document.querySelector('.alert')
 let alertClose = document.querySelector('.alert__close')
 
 if (alertWrapper) {
-  alertWrapper.addEventListener('click', () =>
-    alertWrapper.style.display = 'none'
-  )
+    alertClose.addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log(e);
+        alertWrapper.style.display = 'none';
+    })
 }
